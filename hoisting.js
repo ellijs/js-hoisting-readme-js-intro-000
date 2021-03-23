578c0c21245f4f0ae485d2317e68303cc0305375
ji sun lee <ellijs@hotmail.com>
2021-03-23 01:31:59 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -17,11 +17,11 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-
-  var name = "Cricky";
-
-  sayMy();
-
-
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name)
   }
+  var name = "Cricky";
+  sayMy();
+  
 }
